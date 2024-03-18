4a3bf44558b74941ed8539eed3a621fbaa972130
Cameron <153962889+cr8zedkiwi@users.noreply.github.com>
2024-03-18 04:05:57 -0400

    Add files via upload

---
+++ QueueCollection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> food = new Queue<string>();
+
+
+            food.Enqueue("Pizza");
+            food.Enqueue("Burger");
+            food.Enqueue("Taco");
+            food.Enqueue("Doritos");
+            food.Enqueue("Fries");
+
+
+            string specificItem = "Taco";
+            bool containsItem = food.Contains(specificItem);
+            Console.WriteLine("Does the queue contain tacos?" + containsItem);
+
+            string removedItem = food.Dequeue();
+            Console.WriteLine("Item removed" + removedItem);
+            
+            Console.WriteLine($"Here are your {food.Count()} foods");
+            foreach (var m in food)
+            {
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
