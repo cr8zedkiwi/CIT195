60d079fcb80e29847e3badada3892bba5fa08e22
Cameron <153962889+cr8zedkiwi@users.noreply.github.com>
2024-03-18 04:10:25 -0400

    Add files via upload

---
+++ Lambda Expressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WorkingWithLambdaExpressions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            var x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            var y = Convert.ToDouble(Console.ReadLine());
+
+
+            Func<double, double, double> sum = (double a, double b) => a + b;
+            Func<double, double, double> result = (double a, double b) => a * b;
+
+            var largerValue = Math.Max(x, y);
+
+            Console.WriteLine($"The sum is: {sum(x, y)}");
+            Console.WriteLine($"The multiplication is: {result(x, y)}");
+            Console.WriteLine($"The larger value is: {largerValue}");
+        }
+    }
+}
