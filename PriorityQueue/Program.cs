ceb397cd85db75e1a28c0a4c86c57a5a6dabd6d8
Cameron <153962889+cr8zedkiwi@users.noreply.github.com>
2024-03-18 04:06:30 -0400

    Add files via upload

---
+++ PriorityQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PriorityQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            PriorityQueue<string, int> item = new PriorityQueue<string, int>();
+
+            item.Enqueue("bike", 1);
+            item.Enqueue("shoe", 2);
+            item.Enqueue("pens", 4);
+            item.Enqueue("lamp", 3);
+            item.Enqueue("couch", 5);
+
+
+            while (item.Count > 0)
+            {
+                var highp = item.Peek();
+                Console.WriteLine("Highest priority item:" + highp);
+                highp = item.Dequeue();
+                Console.WriteLine("You have this many items left:" + item.Count);
+            }       
+        }
+    }
+}
